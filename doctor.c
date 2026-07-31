eae8d9a523523e5475a6a76095fad9471c7fc2ab
quad4 <quad4@quadtheoryltd.com>
2026-07-31 17:02:29 +0600

    The functionality of doctor added by Shimu

---
+++ doctor.c
@@ -0,0 +1,44 @@
+
+
+#include "hms.h"
+
+/* ---------------- DOCTOR: limited access ---------------- */
+void doctorRoleMenu(void)
+{
+    int ch;
+    do
+    {
+        printf("\n================ DOCTOR MENU ================\n");
+        printf("1. View Assigned Patients (Dashboard)\n");
+        printf("2. Add Medical Record\n");
+        printf("3. View Medical Records\n");
+        printf("4. Request Lab Test\n");
+        printf("5. View OT Bookings\n");
+        printf("0. Logout\n");
+        printf("Choose: ");
+        ch = readInt();
+        switch (ch)
+        {
+        case 1:
+            doctorDashboard();
+            break;
+        case 2:
+            addMedicalRecord();
+            break;
+        case 3:
+            viewMedicalRecords();
+            break;
+        case 4:
+            requestLabTest();
+            break;
+        case 5:
+            viewOTBookings();
+            break;
+        case 0:
+            printf("Logging out...\n");
+            break;
+        default:
+            printf("Invalid option, please try again.\n");
+        }
+    } while (ch != 0);
+}
